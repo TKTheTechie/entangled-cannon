91a720fce9220199b5c8b60c06874f754a091247
TKTheTechie <thomas.kunnumpurath@solace.com>
2024-01-29 16:58:30 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,8 @@
 </div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+
 	:global(body) {
 		margin: 0;
 		font-family: VT323;
@@ -17,10 +19,5 @@
 		height: 100vh;
 		background: rgb(13, 19, 32);
 		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
-	}
-
-	@font-face {
-		font-family: VT323;
-		src: url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
 	}
 </style>
